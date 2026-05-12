50f7ad2398956a935fabbeb6b49f4c0f68adc454
Atikur Rahman <atikurrahman160313@gmail.com>
2026-05-12 14:35:01 +0600

    refactor: improve token refresh logic and enhance error handling in axios interceptors

--- Frontend/src/api/axios.ts
+++ Frontend/src/api/axios.ts
@@ -1,4 +1,3 @@
-// Frontend/src/api/axios.ts
 import axios, { AxiosError, type InternalAxiosRequestConfig } from "axios";
 import { API_CONFIG } from "../config/api.config";
 
@@ -9,13 +8,14 @@ export const api = axios.create({
   headers: API_CONFIG.HEADERS,
 });
 
-// Token Refresh Queue
+// Token Refresh queue
 let isRefreshing = false;
 let failedQueue: Array<{
   resolve: (value: unknown) => void;
   reject: (reason?: unknown) => void;
 }> = [];
 
+// Process Queue (The result Distributor)
 const processQueue = (error: Error | null, token: string | null = null) => {
   failedQueue.forEach((prom) => {
     if (error) {
@@ -44,6 +44,7 @@ api.interceptors.response.use(
     ) {
       return Promise.reject(error);
     }
+    // Multiple api request handling if its error will be sent this waiting room to be processed
     if (isRefreshing) {
       return new Promise((resolve, reject) => {
         failedQueue.push({ resolve, reject });
@@ -51,25 +52,22 @@ api.interceptors.response.use(
         .then(() => api(originalRequest))
         .catch((err) => Promise.reject(err));
     }
+    // Main refresh logic
     originalRequest._retry = true;
     isRefreshing = true;
-
     try {
       await api.post("/auth/refresh");
       processQueue(null);
       return api(originalRequest);
     } catch (refreshError) {
       processQueue(refreshError as Error);
-
       localStorage.removeItem("user");
-
       if (
         typeof window !== "undefined" &&
         !window.location.pathname.includes("/login")
       ) {
         window.location.href = "/login";
       }
-
       return Promise.reject(refreshError);
     } finally {
       isRefreshing = false;
